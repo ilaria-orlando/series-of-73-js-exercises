cb2fa0cc2372fa85c146c7136b55e716fa87c9bd
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-11-10 14:49:20 +0100

    add exercise 2.6

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -11,11 +11,40 @@
 
 (function() {
 
-    var info = prompt("What is your age, gender and town?");
-
-    if(confirm("Is this correct: " + info)){
-        alert("Thank you!");
-    }else {
-        prompt("What is your age, gender and town?");
+    let age = prompt("What is your age?");
+    let ageconfirm = confirm("Are you " + age + " years old?");
+    if (!ageconfirm){
+        age = prompt("What is your age?");
     }
+    console.log("age");
+    console.log(age);
+
+    let gender = prompt("What is your gender?");
+    let genderconfirm = confirm("Are you a " + gender + "?");
+    if (!genderconfirm){
+        gender = prompt("What is your gender");
+    }
+    console.log("gender");
+    console.log(gender);
+
+    let town = prompt("In which town do you live?");
+    let townconfirm = confirm("Do you live in " + town + "?");
+    if (!townconfirm){
+        town = prompt("In which town do you life?");
+    }
+    console.log("town");
+    console.log(town);
+
+    let result = age + gender + town;
+    let resultconfirm = confirm("Are you a " + age + " years old " + gender + " living in " + town + " ?");
+    if (resultconfirm){
+        alert("Thank you!");
+    }
+    else{
+        location.reload();
+    }
+
+
+
+
 })();
