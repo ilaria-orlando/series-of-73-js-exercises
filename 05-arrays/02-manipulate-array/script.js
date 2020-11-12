e3c802def9dac35cc16ebc154b7573efcf21586b
Ilaria Orlando <ilaria.orlando1991@gmail.com>
2020-11-12 18:02:43 +0100

    add exercise 5.2

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -24,6 +24,10 @@
         "cerise",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function(){
+       fruits[0] = "banana";
+       fruits[9] = "kiwi";
+        console.log(fruits);
+    });
 
 })();
